28083cb0fb0523170aefecee52badfadd46c4a52
JiHoonAHN <ahnjh1028@naver.com>
2022-10-26 20:48:56 +0900

    Update Script

--- Tuist/ProjectDescriptionHelpers/Environment.swift
+++ Tuist/ProjectDescriptionHelpers/Environment.swift
@@ -3,7 +3,6 @@ import ProjectDescription
 public enum Environment {
     public static let appName = ""
     public static let targetName = ""
-    public static let targetDevName = "\(targetName)"
     public static let targetTestName = "\(targetName)Tests"
     public static let organizationName = "JiHoonAHN"
     public static let deploymentTarget: DeploymentTarget = .iOS(targetVersion: "14.0", devices: [.iphone, .ipad])
